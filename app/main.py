47edad5c093a956c623e3430f07db0b139ef1001
DanasPirmasis <alisauskas53@gmail.com>
2026-05-16 20:01:38 +0300

    Added scheduler

--- app/main.py
+++ app/main.py
@@ -1,6 +1,22 @@
+from contextlib import asynccontextmanager
+
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from fastapi import FastAPI
 
+from app.scraper.runner import run_scraper
+
 app = FastAPI()
+
+
+scheduler = AsyncIOScheduler()
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    scheduler.add_job(run_scraper, "cron", hour=2)
+    scheduler.start()
+    yield
+    scheduler.shutdown()  # cleanup on shutdown
 
 
 @app.get("/")
---
+++ app/scraper/runner.py
@@ -0,0 +1,2 @@
+def run_scraper():
+    pass
